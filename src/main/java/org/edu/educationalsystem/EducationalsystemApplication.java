ca96f9ae7cf82308b8c97420a94c5ec9faf1b3e5
Ghost <296776435@qq.com>
2017-12-29 15:34:06 +0800

    project init

---
+++ src/main/java/org/edu/educationalsystem/EducationalsystemApplication.java
@@ -0,0 +1,12 @@
+package org.edu.educationalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalsystemApplication.class, args);
+	}
+}
