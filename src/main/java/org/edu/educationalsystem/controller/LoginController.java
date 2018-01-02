52926968cc375123e74d7bcac0bab18b513a0d5a
Ghost <296776435@qq.com>
2018-01-02 18:48:16 +0800

    login demo

--- src/main/java/org/edu/educationalsystem/controller/LoginController.java
+++ src/main/java/org/edu/educationalsystem/controller/LoginController.java
@@ -2,10 +2,18 @@ package org.edu.educationalsystem.controller;
 
 import org.edu.educationalsystem.Domain.Student;
 import org.edu.educationalsystem.service.LoginService;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -15,12 +23,47 @@ public class LoginController {
     public String index(){
         return "login";
     }
-    @RequestMapping(value = "login")
-    public String login(String user_name,String user_password){
-        if (loginService.login(new Student(user_name,user_password))!=null){
-            return "student";
+//    @RequestMapping(value = "loginVerify")
+//    public String login(String user_name,String user_password){
+//        if (loginService.login(new Student(user_name,user_password))!=null){
+//            return "student";
+//        }
+//        else return "index";
+//    }
+    @RequestMapping(value = "/loginVerify",method = RequestMethod.POST)
+    @ResponseBody
+    public String loginVerify(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map<String, Object> map = new HashMap<String, Object>();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String rememberme = request.getParameter("rememberme");
+        Student student = loginService.login(new Student(username,password));
+        System.out.print(student.getName());
+        if(student==null) {
+            map.put("code",0);
+            map.put("msg","用户名无效！");
+        } else if(!student.getId().equals(password)) {
+            map.put("code",0);
+            map.put("msg","密码错误！");
+        } else {
+            //登录成功
+            map.put("code",1);
+            map.put("msg","");
+            //添加session
+            request.getSession().setAttribute("user", student);
+            //添加cookie
+            if(rememberme!=null) {
+                //创建两个Cookie对象
+                Cookie nameCookie = new Cookie("username", username);
+                //设置Cookie的有效期为3天
+                nameCookie.setMaxAge(60 * 60 * 24 * 3);
+                Cookie pwdCookie = new Cookie("password", password);
+                pwdCookie.setMaxAge(60 * 60 * 24 * 3);
+                response.addCookie(nameCookie);
+                response.addCookie(pwdCookie);
+            }
         }
-        else return "index";
+        String result = new JSONObject(map).toString();
+        return result;
     }
-
 }
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,29 @@
+
+$("#loginForm").submit(function () {
+    var user = $("#user_name").val();
+    var password = $("#user_password").val();
+    if(user=="") {
+        alert("用户名不可为空!");
+    } else if(password==""){
+        alert("密码不可为空!");
+    } else {
+        $.ajax({
+            async: false,//同步，待请求完毕后再执行后面的代码
+            type: "POST",
+            url: 'loginVerify',
+            contentType: "application/x-www-form-urlencoded; charset=utf-8",
+            data: $("#loginForm").serialize(),
+            dataType: "json",
+            success: function (data) {
+                if(data.code==0) {
+                    alert(data.msg);
+                } else {
+                    $("#loginForm").attr("action","index");
+                }
+            },
+            error: function () {
+                alert("数据获取失败")
+            }
+        })
+    }
+})
